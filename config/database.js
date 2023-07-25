24d9a1cbb26ba443c38df525c3d27d2c5f1d7587
Arun kumar <ak2965491@gmail.com>
2023-07-25 18:46:25 +0530

    initial

--- config/database.js
+++ config/database.js
@@ -4,6 +4,6 @@ export const connectDB = async () => {
   mongoose.set('strictQuery', false);
   const { connection } = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB connected with ${connection.host}`);
-};
+};     
 
 
